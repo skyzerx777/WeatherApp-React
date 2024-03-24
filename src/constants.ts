880df111b52bd46455769e8857e1f460af60845f
Yevhenii Hrebenshchykov <yevheniihrebenshchykov@gmail.com>
2024-03-24 09:42:03 +0200

    fixed API url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const API_WEATHER_URL = 'https://api.openweathermap.org/data/2.5';
-export const API_GEOCODING_URL = 'http://api.openweathermap.org/geo/1.0';
+export const API_GEOCODING_URL = 'https://api.openweathermap.org/geo/1.0';
